ddf4bacbedb9793e49f536cf0ecf405bb94b5907
Zachary Daniel <zdaniel827@gmail.com>
2020-04-02 20:55:16 -0700

    file upload

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-const passport = require('passport');
-const config = require('./config');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+const passport = require("passport");
+const config = require("./config");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const campsiteRouter = require('./routes/campsiteRouter');
-const promotionRouter = require('./routes/promotionsRouter');
-const partnerRouter = require('./routes/partnersRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionsRouter");
+const partnerRouter = require("./routes/partnersRouter");
+const uploadRouter = require("./routes/uploadRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -21,40 +22,48 @@ const connect = mongoose.connect(url, {
   useUnifiedTopology: true
 });
 
-connect.then(() => { console.log('connected to server') },
+connect.then(
+  () => {
+    console.log("connected to server");
+  },
   err => console.log(err)
 );
 
 var app = express();
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   if (req.secure) {
     return next();
   } else {
-    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
   }
-})
+});
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
+app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -65,11 +74,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;

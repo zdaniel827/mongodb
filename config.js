817a6d89274426677b9bb9ef0ef87bbe4efa1b59
Zachary Daniel <zdaniel827@gmail.com>
2020-04-03 17:59:32 -0700

    facebook passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
-}
+  secretKey: "12345-67890",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+      clientId: "216454976290565",
+      clientSecret:"c4324bb08ead0ba21c2c42c38cd257fe"
+  }
+};
